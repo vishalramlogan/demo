806dc48625fd554b40527c0d1ad8417d549a0298
Vishal Ramlogan <vishal.ramlogan@my.uwi.edu>
2022-10-31 02:25:53 -0400

    Adding Everyhting

---
+++ routes/api.js
@@ -0,0 +1,51 @@
+const express = require ('express');
+const router = express.Router();
+const Ninja = require('../models/ninja');
+
+// get a list of ninjas from the db
+router.get('/ninjas', function(req, res, next){
+    Ninja.aggregate().near({
+    near: {
+      type: "Point",
+      coordinates: [parseFloat(req.query.lng), parseFloat(req.query.lat)]
+    },
+    maxDistance: 100000, // 300 KM
+    spherical: true,
+    distanceField: "distance"
+  }).then(ninjas => {
+    console.log(ninjas);
+    if (ninjas) {
+      if (ninjas.length === 0)
+        return res.send({
+          message:
+            "maxDistance is too small, or your query params {lng, lat} are incorrect (too big or too small)."
+        });
+      return res.send(ninjas);
+    }
+  }).catch(next);
+});
+
+// add a new ninja to the db
+router.post('/ninjas', function(req, res, next){
+    Ninja.create(req.body).then(function(ninja){
+        res.send(ninja);
+    }).catch(next);
+});
+
+// update a ninja in the db
+router.put('/ninjas/:id', function(req, res, next){
+    Ninja.findByIdAndUpdate({_id: req.params.id}, req.body).then(function(ninja){
+        Ninja.findOne({_id: req.params.id}).then(function(ninja){
+            res.send(ninja);
+        });
+    });
+});
+
+// delete a ninja from the db
+router.delete('/ninjas/:id', function(req, res, next){
+    Ninja.findByIdAndRemove({_id: req.params.id}).then(function(ninja){
+        res.send(ninja);
+    }).catch(next);
+});
+
+module.exports = router;
